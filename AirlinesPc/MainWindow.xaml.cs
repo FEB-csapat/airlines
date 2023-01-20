73db47f0296433b20c474f1e02092e3a28bb3f38
Ruffel1998 <112846348+Ruffel1998@users.noreply.github.com>
2023-01-20 15:03:10 +0100

    Started implementing more details function

--- AirlinesPc/MainWindow.xaml.cs
+++ AirlinesPc/MainWindow.xaml.cs
@@ -68,7 +68,7 @@ namespace AirlinesPc
                 TargetLabel.IsEnabled = false;
             }
         }
-        public void HeaderGenerate()
+        public void HeaderGenerate(int row)
         { 
             int cols=0;
             var label = new Label()
@@ -77,7 +77,7 @@ namespace AirlinesPc
             };
             FlightsGrid.Children.Add(label);
             Grid.SetColumn(label, cols);
-            Grid.SetRow(label, 0);
+            Grid.SetRow(label, row);
             cols++;
             label = new Label()
             {
@@ -85,7 +85,7 @@ namespace AirlinesPc
             };
             FlightsGrid.Children.Add(label);
             Grid.SetColumn(label, cols);
-            Grid.SetRow(label, 0);
+            Grid.SetRow(label, row);
             cols++;
             label = new Label()
             {
@@ -93,7 +93,7 @@ namespace AirlinesPc
             };
             FlightsGrid.Children.Add(label);
             Grid.SetColumn(label, cols);
-            Grid.SetRow(label, 0);
+            Grid.SetRow(label, row);
             cols++;
             label = new Label()
             {
@@ -101,7 +101,7 @@ namespace AirlinesPc
             };
             FlightsGrid.Children.Add(label);
             Grid.SetColumn(label, cols);
-            Grid.SetRow(label, 0);
+            Grid.SetRow(label, row);
             cols++;
             label = new Label()
             {
@@ -109,7 +109,7 @@ namespace AirlinesPc
             };
             FlightsGrid.Children.Add(label);
             Grid.SetColumn(label, cols);
-            Grid.SetRow(label, 0);
+            Grid.SetRow(label, row);
             cols++;
             label = new Label()
             {
@@ -117,7 +117,7 @@ namespace AirlinesPc
             };
             FlightsGrid.Children.Add(label);
             Grid.SetColumn(label, cols);
-            Grid.SetRow(label, 0);
+            Grid.SetRow(label, row);
             cols++;
         }
         public void FlightContent(Flights item,int rows)
@@ -173,7 +173,7 @@ namespace AirlinesPc
             cols++;
             rows++;
         }
-        public void GridGenerate()
+        public void ShowFlights()
         {
             FlightsGrid.Children.Clear();
             FlightsGrid.RowDefinitions.Clear();
@@ -184,9 +184,7 @@ namespace AirlinesPc
             }
             FlightsGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(35, GridUnitType.Pixel) });
             FlightsGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(35, GridUnitType.Pixel) });
-
-            
-            HeaderGenerate();
+            HeaderGenerate(0);
             foreach (var item in FlightRoutes)
             {
                 if (item.StartCity == StartingCity.SelectedValue.ToString() && item.TargetCity == TargetCity.SelectedValue.ToString())
@@ -196,7 +194,127 @@ namespace AirlinesPc
                 }
             }
         }
+        public void Details(Flights item, int rows)
+        {
+            int buttoncounter = 0;
+            int cols = 0;
+            var label = new Label()
+            {
+                Content = item.StartCity
+            };
+            FlightsGrid.Children.Add(label);
+            Grid.SetColumn(label, cols);
+            Grid.SetRow(label, rows);
+            cols++;
+            label = new Label()
+            {
+                Content = item.TargetCity
+            };
+            FlightsGrid.Children.Add(label);
+            Grid.SetColumn(label, cols);
+            Grid.SetRow(label, rows);
+            cols++;
+            label = new Label()
+            {
+                Content = item.AirlineName
+            };
+            FlightsGrid.Children.Add(label);
+            Grid.SetColumn(label, cols);
+            Grid.SetRow(label, rows);
+            cols++;
+            var bt = new Button()
+            {
+                Content = "Bővebb infó",
+                Margin = new Thickness(1,1,1,1),
+                Height = 20,
+                Width = 100,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Bottom
+            };
+            FlightsGrid.Children.Add(bt);
+            Grid.SetColumn(bt, cols);
+            Grid.SetRow(bt, rows);
+            bt.Click += Bt_Click;
+            buttons[buttoncounter] = bt;
+            cols++;
+            rows++;
+        }
+        public void MoreDetails()
+        {
+            FlightsGrid.Children.Clear();
+            FlightsGrid.RowDefinitions.Clear();
+            FlightsGrid.ColumnDefinitions.Clear();
+            FlightsGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(35, GridUnitType.Pixel) });
+            for (int i = 0; i < FlightRoutes.Count; i++)
+            {
+                FlightsGrid.RowDefinitions.Add(new RowDefinition());
+                FlightsGrid.RowDefinitions[i].Height = new GridLength(50, GridUnitType.Pixel);
+            }
+            for (int i = 0; i <= 6; i++)
+            {
+                FlightsGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                FlightsGrid.ColumnDefinitions[i].Width = new GridLength(130, GridUnitType.Pixel);
+            }
+            DetailHeader();
+            for (int i = 0; i < FlightRoutes.Count; i++)
+            {
+                
+                if (i!=currentrow||i+1!=currentrow+1)
+                {
+                    Details(FlightRoutes[i], i + 1);
+                    //TODO:Push list down by two from currentrow, than insert whats in else tree
+                }
+                else
+                {
+                    HeaderGenerate(currentrow + 1);
+
+                    FlightContent(FlightRoutes[currentrow-1], currentrow + 2);
+                }
+            }
+
+        }
+        public void DetailHeader()
+        {
+            int cols = 0;
+            var label = new Label()
+            {
+                Content = "Indul:"
+            };
+            FlightsGrid.Children.Add(label);
+            Grid.SetColumn(label, cols);
+            Grid.SetRow(label, 0);
+            cols++;
+            label = new Label()
+            {
+                Content = "Érkezik:"
+            };
+            FlightsGrid.Children.Add(label);
+            Grid.SetColumn(label, cols);
+            Grid.SetRow(label, 0);
+            cols++;
+            label = new Label()
+            {
+                Content = "Légitársaság neve:"
+            };
+            FlightsGrid.Children.Add(label);
+            Grid.SetColumn(label, cols);
+            Grid.SetRow(label, 0);
+            cols++;
+        }
+        Button[] buttons = new Button[100];
         static readonly Brush RED = new SolidColorBrush(Color.FromRgb(127, 0, 0));
+
+        int currentrow = 0;
+        public void GetPosition(Button button)
+        {
+            currentrow = Grid.GetRow(button);
+        }
+        private void Bt_Click(object sender, RoutedEventArgs e)
+        {
+            GetPosition(sender as Button);
+            Button currentButton = buttons[currentrow];
+            MoreDetails();
+        }
         public void ListAllFlights()
         {
             FlightsGrid.Children.Clear();
@@ -212,15 +330,15 @@ namespace AirlinesPc
                 FlightsGrid.ColumnDefinitions.Add(new ColumnDefinition());
                 FlightsGrid.ColumnDefinitions[i].Width = new GridLength(130, GridUnitType.Pixel);
             }
-            HeaderGenerate();
+            DetailHeader();
             for (int i = 0; i < FlightRoutes.Count; i++)
             {
-                FlightContent(FlightRoutes[i], i + 1);
+                Details(FlightRoutes[i], i + 1);
             }
         }
         private void TargetCity_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            GridGenerate();
+            ShowFlights();
         }
 
         private void ShowAllFlights_Click(object sender, RoutedEventArgs e)
