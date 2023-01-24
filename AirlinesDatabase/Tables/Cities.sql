9e383ade64bb7bbe4bfc9671e963f82a76c998ec
Isakov <isakov.erik20021@gmail.com>
2023-01-24 11:56:33 +0100

    Removed unused projects

--- AirlinesDatabase/Tables/Cities.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Cities]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] TEXT NOT NULL, 
-    [Population] INT NOT NULL
-)
--- AirlinesDatabase/Tables/dbo.Cities.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Cities] (
-    [Id]         INT        IDENTITY (1, 1) NOT NULL,
-    [Name]       TEXT NOT NULL,
-    [Population] INT        NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
