1bc96bdc0fde845e44c6f31628088ba2fbe44616
Ruffel1998 <112846348+Ruffel1998@users.noreply.github.com>
2023-01-24 15:45:52 +0100

    Added cities with population to database

--- Database/Database/Context.cs
+++ Database/Database/Context.cs
@@ -1,6 +1,7 @@
 ﻿using Database.Database.Model;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
 
 
 namespace Database.Database
@@ -59,8 +60,122 @@ namespace Database.Database
                 new City
                 {
                     Id = 1,
+                    Name = "Budapest",
+                    Population = 1800000
+                },
+                new City
+                {
+                    Id = 2,
+                    Name = "London",
+                    Population = 8800000
+                },
+                new City
+                {
+                    Id = 3,
+                    Name = "Szarajevó",
+                    Population = 3500000
+                },
+                new City
+                {
+                    Id = 4,
+                    Name = "Sao Paulo",
+                    Population = 12325232
+                },
+                new City
+                {
+                    Id = 5,
+                    Name = "Minszk",
+                    Population = 9400000
+                },
+                new City
+                {
+                    Id = 6,
+                    Name = "Asztana",
+                    Population = 16700000
+                },
+                new City
+                {
+                    Id = 7,
+                    Name = "Párizs",
+                    Population = 2165423
+                },
+                new City
+                {
+                    Id = 8,
+                    Name = "Belgrád",
+                    Population = 1378682
+                },
+                new City
+                {
+                    Id = 9,
+                    Name = "Bogotá",
+                    Population = 7743955
+                },
+                new City
+                {
+                    Id = 10,
+                    Name = "Dakar",
+                    Population = 1438725
+                },
+                new City
+                {
+                    Id = 10,
+                    Name = "Havanna",
+                    Population = 2141652
+                },
+                new City
+                {
+                    Id = 10,
+                    Name = "Libreville",
+                    Population = 797003
+                },
+                new City
+                {
+                    Id = 10,
                     Name = "Luxembourg",
-                    Population = 100000
+                    Population = 132780
+                },
+                new City
+                {
+                    Id = 10,
+                    Name = "Madrid",
+                    Population = 3280782
+                },
+                new City
+                {
+                    Id = 10,
+                    Name = "Manila",
+                    Population = 1846513
+                },
+                new City
+                {
+                    Id = 10,
+                    Name = "Mexikóváros",
+                    Population = 8851080
+                },
+                new City
+                {
+                    Id = 10,
+                    Name = "Moszkva",
+                    Population = 12455682
+                },
+                new City
+                {
+                    Id = 10,
+                    Name = "Peking",
+                    Population = 21893095
+                },
+                new City
+                {
+                    Id = 10,
+                    Name = "Pozsony",
+                    Population = 475503
+                },
+                new City
+                {
+                    Id = 10,
+                    Name = "Prága",
+                    Population = 1275406
                 }
             );
             modelBuilder.Entity<Airline>().HasData(
