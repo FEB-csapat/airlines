a0bfc62db3cfd5f11e8d2e3e5a7a6c76d2dd5992
Isakov <isakov.erik20021@gmail.com>
2023-01-25 23:29:44 +0100

    App.cs add, AirlinesController tests add

---
+++ AirlinesApi/App.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+
+namespace AirlinesApi
+{
+    public class App
+    {
+        static WebApplicationBuilder builder;
+        static WebApplication app;
+
+        private static void Initialize(string[]? args)
+        {
+            builder = WebApplication.CreateBuilder(args ?? new string[0]);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+            app = builder.Build();
+
+            app.UseCors(builder => builder
+             .AllowAnyOrigin()
+             .AllowAnyMethod()
+             .AllowAnyHeader());
+            // app.UseMvc();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+
+        public static void RunDev(string[]? args = null)
+        {
+            Initialize(args);
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.Run();
+        }
+
+        public static void RunTest(string[]? args = null)
+        {
+            Initialize(args);
+            app.Run();
+        }
+    }
+}
