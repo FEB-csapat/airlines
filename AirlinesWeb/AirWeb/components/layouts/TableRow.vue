0c54c23ace7e8fbb17674b3cc1223eba31c69010
-m <-m>
2023-01-25 07:17:46 +0100

    Dynamic data listing with test data in tables

---
+++ AirlinesWeb/AirWeb/components/layouts/TableRow.vue
@@ -0,0 +1,25 @@
+<template>
+<tr>
+    <td>{{tarsasag}}</td>
+    <td>{{kiindulas}}</td>
+    <td>{{celvaros}}</td>
+    <td>{{tavolsag}}</td>
+    <td>{{idotartam}}</td>
+    <td>{{kmar}}</td>
+    <td><button class="btn btn-success" @click="$emit('addToCart',id)">asd</button></td>
+</tr>
+</template>
+<script>
+import { RouterLink } from 'vue-router';
+export default{
+    props:{
+        id: Number,
+        tarsasag: String,
+        kiindulas: String,
+        celvaros: String,
+        tavolsag: Number,
+        idotartam: Number,
+        kmar: Number
+    }
+}
+</script>
