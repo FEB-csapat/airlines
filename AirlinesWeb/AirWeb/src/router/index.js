7efef5bb23f47ebc3535ee3b45c58a2cb60ceeee
-m <-m>
2023-01-25 05:04:35 +0100

    Base site, with navbar created

---
+++ AirlinesWeb/AirWeb/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import IndexView from "@/views/IndexView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      name:'Index',
+      path:'/',
+      component: IndexView
+    }
+  ]
+
+})
+
+export default router
