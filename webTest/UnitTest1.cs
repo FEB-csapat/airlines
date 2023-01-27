394b837a05f5258461fe92211e672bbaf8678716
Ruffel1998 <112846348+Ruffel1998@users.noreply.github.com>
2023-01-27 02:14:18 +0100

    Added selenium tests

--- webTest/UnitTest1.cs
+++ webTest/UnitTest1.cs
@@ -1,4 +1,4 @@
-using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 namespace webTest
 {
@@ -16,19 +16,67 @@ namespace webTest
                 driver.Url = "http://localhost:5173";
             }
             [TestMethod]
-            public void TestMethod1()
+            public void CartAddTest()
             {
-                var asd = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[2]"));
-                Assert.AreEqual("amerika", asd.Text);
-                var button = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[7]/button"));
-                button.Click();
+                var tocity = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[3]"));
+                Assert.AreEqual("London", tocity.Text);
+                var fromcity = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[2]"));
+                Assert.AreEqual("Budapest", fromcity.Text);
+                var airline = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[1]"));
+                Assert.AreEqual("WizzAir", airline.Text);
+                var tocity1 = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[4]/td[3]"));
+                Assert.AreEqual("Szarajevó", tocity1.Text);
+                var fromcity1 = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[4]/td[2]"));
+                Assert.AreEqual("Belgrád", fromcity1.Text);
+                var airline1 = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[4]/td[1]"));
+                Assert.AreEqual("Air Serbia", airline1.Text);
+                var tocartbutton = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[7]/button"));
+                tocartbutton.Click();
+                driver.SwitchTo().Alert().Accept();
+                var tocartbutton1 = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[4]/td[7]/button"));
+                tocartbutton1.Click();
                 driver.SwitchTo().Alert().Accept();
                 driver.Url = "http://localhost:5173/cart";
-                var asd1 = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr/td[2]"));
-                Assert.AreEqual("amerika", asd1.Text);
+                var cartfromcity = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr/td[2]"));
+                Assert.AreEqual("Budapest", cartfromcity.Text);
+                var cartfromcity1 = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[2]/td[2]"));
+                Assert.AreEqual("Belgrád", cartfromcity1.Text);
+                var carttocity = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[3]"));
+                Assert.AreEqual("London", carttocity.Text);
+                var carttocity1 = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[2]/td[3]"));
+                Assert.AreEqual("Szarajevó", carttocity1.Text);
+            }
+            [TestMethod]
+            public void CartDeleteTest()
+            {
+                var tocartbutton = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[7]/button"));
+                tocartbutton.Click();
+                driver.SwitchTo().Alert().Accept();
+                var tocartbutton1 = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[4]/td[7]/button"));
+                tocartbutton1.Click();
+                driver.SwitchTo().Alert().Accept();
+                driver.Url = "http://localhost:5173/cart";
+                var deletebutton = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[5]/button"));
+                deletebutton.Click();
+                var cartfromcity = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr/td[2]"));
+                Assert.AreEqual("Belgrád", cartfromcity.Text);
+                var carttocity = driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[3]"));
+                Assert.AreEqual("Szarajevó", carttocity.Text);
+            }
+            [TestMethod]
+            public void NavBarTest()
+            {
+                var title = driver.FindElement(By.XPath("/html/body/div/div/div/h2"));
+                Assert.AreEqual("Menetrend keresõ", title.Text);
+                driver.Url = "http://localhost:5173/cart";
+                var carttitle = driver.FindElement(By.XPath("/html/body/div/div/div/h2"));
+                Assert.AreEqual("A jegyek végösszege", carttitle.Text);
+                driver.Url = "http://localhost:5173";
+                title = driver.FindElement(By.XPath("/html/body/div/div/div/h2"));
+                Assert.AreEqual("Menetrend keresõ", title.Text);
             }
         }
 
-        
+
     }
 }
